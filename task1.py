d044e7d4a9e17880b17d366d06dacbd92df24073
piposohib <sohibahmedeldeb@gmail.com>
2025-09-02 18:49:26 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+temperature=5
+if temperature >=30 :
+    print("It's a hot day. stay hydrated!")
+elif 29>=temperature>=20 :
+    print("It's a warm day. Enjoy the weather!")
+elif 19>=temperature>=10 :
+    print("It's a cool day. Wear a jacket!")
+elif temperature<10 :
+    print("It's a cold day. stay warm!")
+for i in range(1,21):
+    if i%3==0:
+        print(i)
+    
+    
